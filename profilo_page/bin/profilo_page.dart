d225f6232e4a00e364296ad126d842030b30493d
GroundedPhysics <updated200@gmail.com>
2022-06-20 10:43:48 +0430

    test commit

--- profilo_page/bin/profilo_page.dart
+++ profilo_page/bin/profilo_page.dart
@@ -1,5 +1,5 @@
 import 'package:profilo_page/profilo_page.dart' as profilo_page;
 
 void main(List<String> arguments) {
-  print('Hello world: ${profilo_page.calculate()}!');
+  print('Salam : ${profilo_page.calculate()}!');
 }
